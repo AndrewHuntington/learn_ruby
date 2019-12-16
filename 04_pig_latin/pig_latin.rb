aa93f8c1d07b8a48e6caad3c92fe8d2226c79636
AndrewHuntington <andrew.huntington@gmail.com>
2019-12-16 18:47:50 +0900

    solve 04 up to bonus problems

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,35 @@
-#write your code here
+def translate phrase
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  special_cases = ['qu']
+
+  phrase.split().map { |word|
+    caps = false
+
+    if word == word.capitalize
+      caps = true
+    end
+
+    case true
+    when vowels.include?(word[0].to_s.downcase)
+      word = word + "ay"
+    when special_cases.include?(word[0..1].to_s.downcase)
+      word = word[2..] + "quay"
+    when special_cases.include?(word[1..2].to_s.downcase)
+      word = word[3..] + word[0] + "quay"
+    when !vowels.include?(word[0].to_s.downcase) && !vowels.include?(word[1].to_s.downcase) && !vowels.include?(word[2].to_s.downcase)
+      word = word[3..] + word[0..2] + "ay"
+    when !vowels.include?(word[0].to_s.downcase) && !vowels.include?(word[1].to_s.downcase)
+      word = word[2..] + word[0..1] + "ay"
+    when !vowels.include?(word[0].to_s.downcase)
+      word = word[1..] + word[0] + "ay"
+    else
+      "Error!"
+    end
+
+    # # this causes problems
+    # if caps
+    #   p word.capitalize
+    # end
+  }.join(" ")
+
+end
