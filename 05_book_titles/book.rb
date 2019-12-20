be67d52725d7e6a78026f092ca0d53e50b896e54
AndrewHuntington <andrew.huntington@gmail.com>
2019-12-20 23:56:50 +0900

    solve 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  @@ng_words = ['and', 'in', 'the', 'of', 'a', 'an']
+
+  # setter
+  def title=(title)
+    @title = title.split.map { |x|
+      if !@@ng_words.include?(x)
+        x.capitalize
+      else
+        x
+      end
+    }.join(" ")
+  end
+
+  # getter
+  def title
+    @title[0].upcase + @title[1..@title.length]
+  end
+
 end
